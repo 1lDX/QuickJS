9187b9e010e577759dcd22c6994a860a9b287357
1lDX <worrapon453@gmail.com>
2024-07-08 01:25:00 +0700

    first commit

---
+++ Note/note.js
@@ -0,0 +1,32 @@
+//check comment command
+
+document.write("<h1>Larmar.com</h1>");
+document.write("<p>ทดสอบ Javascript เบื้องต้น<p>");
+/* 
+check comment command
+*/
+alert("Hello Larmar");
+console.log("Hello JarvaScript");
+console.error("Virus has been detected");
+console.warn("Fiding mistake");
+
+//variable in javarscript is case sensitive
+//JarvaScript is Dynamic Typing
+
+
+//Let (เปลี่ยนแปลงค่าในคัวแปรได้)
+let money;
+let moeny =100;
+money = 200;
+let a,b,c,d,e;
+let x =10,y=20,z=30;
+
+
+//const =(ค่าคงที่เปลี่ยนแปลงไม่ได้)
+const money2 =100;
+money2 =200; //เปลี่ยนแปลงค่าเดิมไม่ได้
+
+//ใช้ $ (dollar-sign)หรือ _ ในการตั้งตัวแปรได้ = $money, -money
+$money = 200;
+_tech = "teach me pls";
+console.log(_tech);
---
+++ app..js
@@ -0,0 +1,20 @@
+let money = 500;//number int
+let price = 99.99//number (float)
+let message ='CS39'
+let name ="Larmar"
+
+
+let checkname = true
+
+console.log(money)
+console.log(price)
+console.log(message)
+console.log(name)
+console.log(checkname);
+
+
+console.log(typeof(money))
+console.log(typeof(price))
+console.log(typeof(message))
+console.log(typeof(name))
+console.log(typeof(checkname))
